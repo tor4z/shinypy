867a7d36494be4d6d59ae294d717b0102a133d39
tor4z <vwenjie@hotmail.com>
2018-05-18 03:33:50 +0800

    add const

---
+++ shiny/const.py
@@ -0,0 +1,8 @@
+import os
+
+
+assets = ("/assets", os.path.join(os.path.dirname(__file__), "assets"))
+
+bootstrap_css_path = "/assets/library/css/bootstrap.min.css"
+bootstrap_js_path = "/assets/library/js/bootstrap.bundle.min.js"
+jquery_js_path = "/assets/library/js/jquery.min.js"
