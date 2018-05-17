f7ce206bddc13cf56a3f2eae6e3954244e97c644
tor4z <vwenjie@hotmail.com>
2018-05-18 03:34:16 +0800

    Update basic example

--- examples/basic.py
+++ examples/basic.py
@@ -1,9 +1,13 @@
 from shiny.ui import UI
 from shiny.server import Server
 from shiny.app import App
+from shiny.html5 import Element
+
 
 ui = UI()
 server = Server()
+
+ui.append(Element("div"))
 
 if __name__ == "__main__":
     app = App(ui, server)
