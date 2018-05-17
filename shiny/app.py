19c9db36a3aefdb35575b962c1e654413fd4dbe0
tor4z <vwenjie@hotmail.com>
2018-05-18 03:33:38 +0800

    Add basic app

--- shiny/app.py
+++ shiny/app.py
@@ -1,3 +1,20 @@
+from aiohttp import web
+from .const import assets
+
+
 class App:
-    def start(self):
-        pass
+    def __init__(self, ui, server):
+        self.ui = ui
+        self.server = server
+        self.app = web.Application()
+        self.app.add_routes([web.get('/', self.handle)])
+        print(assets)
+        self.app.router.add_static(assets[0], assets[1])
+
+    async def handle(self, request):
+        name = request.match_info.get('name', "Anonymous")
+        headers = {"Content-Type": "text/html"}
+        return web.Response(text=str(self.ui), headers=headers)
+
+    def start(self, port=None):
+        web.run_app(self.app)
