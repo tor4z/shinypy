48c69874f75b2aa44ea271679bdaf457848776a3
tor4z <vwenjie@hotmail.com>
2018-05-18 05:55:43 +0800

    Update const

--- shiny/const.py
+++ shiny/const.py
@@ -6,3 +6,5 @@ assets = ("/assets", os.path.join(os.path.dirname(__file__), "assets"))
 bootstrap_css_path = "/assets/library/css/bootstrap.min.css"
 bootstrap_js_path = "/assets/library/js/bootstrap.bundle.min.js"
 jquery_js_path = "/assets/library/js/jquery.min.js"
+
+dashboard_css_path = "/assets/layout/dashboard/css/style.css"
