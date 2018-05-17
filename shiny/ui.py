3a2dec8cefb781b06126386987954adfc0c38a61
tor4z <vwenjie@hotmail.com>
2018-05-18 03:33:28 +0800

    Update UI

--- shiny/ui.py
+++ shiny/ui.py
@@ -1,2 +1,42 @@
+from .html5 import Element, CSS, JS, Title
+from .const import (bootstrap_css_path,
+                    bootstrap_js_path,
+                    jquery_js_path)
+
+
 class UI:
-    pass
+    _TITLE = "Shiny App"
+
+    def __init__(self, title=None):
+        self.html = Element("html", declarar=True)
+        self.title = title
+        self.body = None
+        self.init_head()
+        self.init_body()
+
+    def init_head(self):
+        head = Element("head")
+
+        title = Title(self.title or self._TITLE)
+        bootstrap_css= CSS(bootstrap_css_path)
+        bootstrap_js = JS(bootstrap_js_path)
+        jquery_js = JS(jquery_js_path)
+
+        head.append(title)
+        head.append(bootstrap_css)
+        head.append(bootstrap_js)
+        head.append(jquery_js)
+
+        self.html.append(head)
+
+    def init_body(self):
+        self.body = Element("body")
+
+    def append(self, ele):
+        self.body.append(ele)
+
+    def __str__(self):
+        self.html.append(self.body)
+        return str(self.html)
+
+    __repr__ = __str__
