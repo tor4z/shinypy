e8dda992130be549baea47819f81129e36bd0e7d
tor4z <vwenjie@hotmail.com>
2018-05-18 21:48:50 +0800

    USE bs4

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -1,7 +1,15 @@
 from shiny import widgets as wg
+from shiny.util import randstr
+from bs4 import BeautifulSoup
 
 
 def test_lable():
-    label = wg.Label()
-    label.value("hello")
-    # TODO assert
+    id = randstr(5)
+    val = randstr(5)
+    label = wg.Label(id, val)
+    soup = BeautifulSoup(str(label), "lxml")
+    tag = soup.div
+
+    assert tag.name == "div"
+    assert tag["id"] == id
+    assert tag.text == val
