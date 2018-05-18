6ccb99d53441824fb769c3bc71c4f8cccc57c003
tor4z <vwenjie@hotmail.com>
2018-05-18 22:19:31 +0800

    Add layout unit test

---
+++ tests/test_layout.py
@@ -0,0 +1,19 @@
+from shiny.layout import DashboardLayout
+from bs4 import BeautifulSoup
+from shiny.util import randstr
+
+
+def test_dashboard():
+    name = randstr(5)
+    dbl = DashboardLayout(name)
+
+    soup = BeautifulSoup(str(dbl), "lxml")
+    navbar = soup.div.nav
+    container = soup.find("div", class_="container-fluid")
+    sideabr = container.find("nav", class_="sidebar")
+    main = container.find("main")
+
+    assert navbar.name == "nav"
+    assert container.name == "div"
+    assert sideabr.name == "nav"
+    assert main.name == "main"
