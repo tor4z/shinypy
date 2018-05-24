9889c86f883d9e8ae2cc85b355a578873a35199c
tor4z <vwenjie@hotmail.com>
2018-05-24 18:02:34 +0800

    Add bootstrap map

--- shiny/const.py
+++ shiny/const.py
@@ -6,5 +6,6 @@ assets = ("/assets", os.path.join(os.path.dirname(__file__), "assets"))
 bootstrap_css_path = "/assets/library/css/bootstrap.min.css"
 bootstrap_js_path = "/assets/library/js/bootstrap.bundle.min.js"
 jquery_js_path = "/assets/library/js/jquery.min.js"
+shiny_js_path = "/assets/shiny.js"
 
 dashboard_css_path = "/assets/layout/dashboard/css/style.css"
