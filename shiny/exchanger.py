d4ad17c64dab3a3c4f168060565627e6d2f3507e
tor4z <vwenjie@hotmail.com>
2018-05-26 16:47:04 +0800

    Redo exchange

--- shiny/exchanger.py
+++ shiny/exchanger.py
@@ -1,51 +1,81 @@
-class Exchanger:
-    def __init__(self, wstream):
-        self._wstream = wstream
+from .wstream import string_to_json, json_to_string
+
+
+class Mapping:
+    def __init__(self):
+        # TODO double end binding
+        self._inited = []
         self._mapping = {}
-        self._tasks = []
 
-    def register(self, out_port):
-        self._mapping[out_port] = []
+    def set(self, in_port, out_port):
+        if out_port in self._inited:
+            return
+        else:
+            self._inited.append(out_port)
 
-    def subscribe(self, out_port, in_port):
-        self._mapping[out_port].append(in_port)
+        if in_port in self._mapping:
+            self._mapping[in_port].add(out_port)
+        else:
+            self._mapping[in_port] = {out_port}
 
-    async def get(self, key):
-        return await self._wstream.get(key)
-
-    async def consume(self):
-        task, out_port = self._tasks.pop()
-        result = task()
-        data = {out_port: result}
-        await self._wstream.send_dict(data)
-
-    def start(self):
-        pass
+    def get_outs(self, in_port):
+        return self._mapping[in_port]
 
 
 class In:
-    def __init__(self, exchanger):
-        self._exchanger = exchanger
+    def __init__(self, msg, mapping, ws):
         self._out_port = None
-
-    def register(self, out_port):
-        self._exchanger.register(out_port)
-        self._out_port = out_port
+        self._data = string_to_json(msg)
+        self.mapping = mapping
+        self._ws = ws
 
     def __setitem__(self, key, value):
         raise NotImplementedError
 
-    async def __getitem__(self, key):
-        self._exchanger.subscribe(self._out_port, key)
-        return await self._exchanger.get(key)
+    def __getitem__(self, in_port):
+        self.mapping.set(in_port, self._out_port)
+        if in_port in self._data:
+            return self._data[in_port]
+        else:
+            # Get data from client
+            # Cache data
+            pass
+
+    @property
+    def in_ports(self):
+        return self._data.keys()
+
+    def instance(self, out_port):
+        self._out_port = out_port
+        return self
 
 
 class Out:
-    def __init__(self, wstream):
-        self._wstream = wstream
+    def __init__(self, iner):
+        self._in = iner
+        self.mapping = iner.mapping
+        self._outs = {}
 
-    async def __setitem__(self, key, func):
-        await self._wstream.send_dict({key: func()})
+    def __setitem__(self, key, func):
+        self._outs[key] = func
 
     def __getitem__(self, key):
         raise NotImplementedError
+
+    def _out_data(self):
+        # TODO lazy I/O
+        data = {}
+        out_ports = set()
+        for in_port in self._in.in_ports:
+            out_ports.union(self.mapping.get_outs(in_port))
+
+        for out_port in out_ports:
+            func = self._outs[out_port]
+            iner = self._in.instance(out_port)
+            data[out_port] = func(iner)
+        return data
+
+    def __str__(self):
+        return json_to_string(self._out_data())
+
+    __repr__ = __str__
