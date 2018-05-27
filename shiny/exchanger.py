d4ab6231ce16ba9cdfbe0dd62147dfb7e22b3fd6
tor4z <vwenjie@hotmail.com>
2018-05-28 01:03:33 +0800

    Update imp lazy I/O

--- shiny/exchanger.py
+++ shiny/exchanger.py
@@ -1,4 +1,4 @@
-from .wstream import string_to_json, json_to_string
+from .message import Message
 
 
 class Mapping:
@@ -28,21 +28,24 @@ class Mapping:
 class In:
     def __init__(self, msg, mapping, ws):
         self._out_port = None
-        self._data = string_to_json(msg)
+        self._data = msg.data
         self.mapping = mapping
         self._ws = ws
 
     def __setitem__(self, key, value):
         raise NotImplementedError
 
-    def __getitem__(self, in_port):
+    async def __getitem__(self, in_port):
         self.mapping.bind(in_port, self._out_port)
-        if in_port in self._data:
-            return self._data[in_port]
-        else:
-            # Get data from client
-            # Cache data
-            pass
+        if in_port not in self._data:
+            data = await self._ws.query(in_port)
+            self._data.update(data)
+        return self._data[in_port]
+
+    async def cache(self, in_ports):
+        if in_ports:
+            data = await self._ws.query(in_ports)
+            self._data.update(data)
 
     @property
     def in_ports(self):
@@ -58,6 +61,7 @@ class Out:
         self._in = iner
         self.mapping = iner.mapping
         self._outs = {}
+        self._data = {}
 
     def __setitem__(self, key, func):
         self._outs[key] = func
@@ -65,20 +69,23 @@ class Out:
     def __getitem__(self, key):
         raise NotImplementedError
 
-    def _out_data(self):
-        # TODO lazy I/O
-        data = {}
+    async def execute(self):
         out_ports = set()
+        in_ports = set()
         for in_port in self._in.in_ports:
             out_ports.union(self.mapping.get_outs(in_port))
 
         for out_port in out_ports:
+            in_ports.union(self.mapping.get_ins(out_port))
+
+        await self._in.cache(in_ports)
+        for out_port in out_ports:
             func = self._outs[out_port]
             iner = self._in.instance(out_port)
-            data[out_port] = func(iner)
-        return data
+            self._data[out_port] = func(iner)
 
-    def __str__(self):
-        return json_to_string(self._out_data())
-
-    __repr__ = __str__
+    @property
+    def msg(self):
+        msg = Message()
+        msg.data = self._data
+        return msg
