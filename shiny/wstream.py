b91292bf42c57034221143634d86071353200303
tor4z <vwenjie@hotmail.com>
2018-05-28 01:04:26 +0800

    Remove Message

--- shiny/wstream.py
+++ shiny/wstream.py
@@ -1,8 +1,9 @@
-import json
 from enum import IntEnum
+from .message import Message
+from .util import json_to_string, string_to_json
 
 
-class MsgType(IntEnum):
+class WSMsgType(IntEnum):
     # websocket spec types
     CONTINUATION = 0x0
     TEXT = 0x1
@@ -18,6 +19,7 @@ class MsgType(IntEnum):
 
     # shiny specific types
     DICT = 0x103
+    MSG = 0x104
 
     text = TEXT
     binary = BINARY
@@ -28,25 +30,10 @@ class MsgType(IntEnum):
     closed = CLOSED
     error = ERROR
     dict = DICT
+    msg = MSG
 
 
-class Status(IntEnum):
-    SUCCESS = 0x0
-    ERROR = 0x1
-
-    success = SUCCESS
-    error = ERROR
-
-
-def json_to_string(data):
-    return json.dumps(data)
-
-
-def string_to_json(string):
-    return json.loads(string)
-
-
-class Wstream:
+class WStream:
     def __init__(self, resp):
         self._ws = resp
 
@@ -55,35 +42,47 @@ class Wstream:
 
     async def __anext__(self):
         try:
-            msg, type = await self.receive_data()
+            msg, type = await self.receive_msg()
             return msg, type
-        except WstreamExp:
+        except WStreamExp:
             raise StopAsyncIteration
 
-    async def get(self, key):
-        data = MessageParser.get(key)
-        await self.send_data(data)
-        data, type = await self.receive_data()
-        return MessageParser.value(data)
+    async def query(self, keys):
+        msg = Message()
+        msg.query(keys)
+        await self.send_msg(msg)
+        result, _ = await self.receive_msg()
+        return result.data
+
+    async def receive_msg(self, *args, **kwargs):
+        string, type = await self.receive(*args, **kwargs)
+        if type is WSMsgType.TEXT:
+            msg = Message(string)
+            return msg, msg.MSG
 
     async def receive_data(self, *args, **kwargs):
         msg, type = await self.receive(*args, **kwargs)
-        if type is MsgType.TEXT:
+        if type is WSMsgType.TEXT:
             data = string_to_json(msg.data)
             return data, msg.DICT
 
     async def receive(self, *args, **kwargs):
         msg = await self._ws.receive(*args, **kwargs)
-        if msg.type in (MsgType.CLOSE,
-                        MsgType.CLOSING,
-                        MsgType.CLOSED):
-            raise WstreamExp("WebSocket closed.")
+        if msg.type in (WSMsgType.CLOSE,
+                        WSMsgType.CLOSING,
+                        WSMsgType.CLOSED):
+            raise WStreamExp("WebSocket closed.")
         return msg.data, msg.type
 
     async def send_str(self, string):
         if not isinstance(string, str):
             raise TypeError("str required.")
         await self._ws.send_str(string)
+
+    async def send_msg(self, msg):
+        if not isinstance(msg, Message):
+            raise TypeError("Message required.")
+        await self._ws.send_str(str(msg))
 
     async def send_data(self, data):
         if not isinstance(data, dict):
@@ -99,92 +98,5 @@ class Wstream:
         return self._ws
 
 
-class MessageParser:
-    '''
-    All message should be a json string.
-    Get:
-        query msg:
-        {
-            method: 'GET',
-            data: {
-                keys: ['key1', 'key2', 'key3', ...]
-            }
-        }
-        responses msg:
-        {
-            status: status_code,
-            reason: 'Error message',
-            data: {
-                key: value (single value or array of value)
-            }
-        }
-
-    Set:
-        set msg
-        {
-            method: 'SET',
-            data: {}
-        }
-        responses msg:
-        {
-            status: status_code,
-            reason: 'Error message',
-        }
-
-    Execute:
-        {
-            method: 'EXEC',
-            data: {}
-        }
-
-        {
-            status: status_code,
-            reason: 'Error message',
-            data: {}
-        }
-    '''
-    GET = 'GET'
-    SET = 'SET'
-    EXEC = 'EXEC'
-
-    @classmethod
-    def get(cls, *keys):
-        msg = {}
-        msg['method'] = cls.GET
-        msg['data'] = {'keys': list(keys)}
-        return json_to_string(msg)
-
-    def __init__(self, msg):
-        if isinstance(msg, str):
-            msg = string_to_json(msg)
-        self.msg = msg
-
-    @property
-    def status(self):
-        return self.msg.get("status")
-
-    @property
-    def reason(self):
-        return self.msg.get("reason")
-
-    @property
-    def method(self):
-        return self.msg.get("method")
-
-    @property
-    def keys(self):
-        if self.method == self.GET:
-            return self.data.get("keys")
-        else:
-            return self.data.keys()
-
-    @property
-    def data(self):
-        return self.msg.get("data")
-
-    def value(self, key):
-        return self.data.get(key)
-
-
-class WstreamExp(Exception):
+class WStreamExp(Exception):
     pass
