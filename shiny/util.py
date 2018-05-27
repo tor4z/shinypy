8931282b5601664f4ba0d4e2067b33571bf3812e
tor4z <vwenjie@hotmail.com>
2018-05-28 01:04:01 +0800

    Add json string transform function

--- shiny/util.py
+++ shiny/util.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 
 _DIGIT = '0123456789'
@@ -9,3 +10,11 @@ _PRINTABLE = _DIGIT + _ALAPHA
 def randstr(k=5):
     k = k-1
     return '_' + ''.join(random.choices(_PRINTABLE, k=k)) if k > 0 else ''
+
+
+def json_to_string(data):
+    return json.dumps(data)
+
+
+def string_to_json(string):
+    return json.loads(string)
