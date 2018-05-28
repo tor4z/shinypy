60a0dcf1aaad7528ccd54583949950a6156d8c4e
tor4z <vwenjie@hotmail.com>
2018-05-28 18:14:34 +0800

    Update example

--- examples/basic.py
+++ examples/basic.py
@@ -8,16 +8,19 @@ ui = UI()
 dashboard = DashboardLayout("Test")
 ui.layout = dashboard
 dashboard.sidebar.append(
-    wg.Text("test", "test", label="Test"))
+    wg.Text("test", "test", label="Test"),
+    wg.Password('passwd', label="Password"),
+    wg.Checkbox('checkbox', [('option1', 'Option1', True),
+                             ('option2', 'Option2')]))
 dashboard.main.append(
     wg.Label("test"))
 
 
 def server(OUT):
     @wg.Label.render
-    def do_a(IN):
+    def do_test(IN):
         return IN["test"]
-    OUT["test"] = do_a
+    OUT["test"] = do_test
 
 
 if __name__ == "__main__":
